c0f81db4eb8ff85bac0beff008d00f3eb57c25de
Febri Nahdatun <febri.nahdatun@gmail.com>
2026-04-19 13:49:38 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@
    CONFIG — Ganti API_URL setelah deploy Apps Script
    ============================================ */
 const CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbxlmpe14bljoph0mO_BYpOodzJ9O_EI0iwuqT9CDd8JJcluJxHn1GmkFEElJv4vxvcJQw/exec",
   DEFAULT_AVATAR: "https://ui-avatars.com/api/?background=0f172a&color=38bdf8&size=400&font-size=0.35&bold=true&name=",
 };
-// Deploy di PythonAnywhere: https://USERNAME.pythonanywhere.com
